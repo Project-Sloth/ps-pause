a6743f6d1cf631fa8aca36d995c37143b90912bf
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-12-24 09:12:22 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ps-pause
+Changes the background of the pause menu
+# Install
+* Drag and drop
+* Ensure ps-pause
---
+++ client.lua
@@ -0,0 +1,32 @@
+--[[
+	background_blue
+	background_darkblue
+	background_darkerblue
+	background_darkgreen
+	background_green
+	background_other
+	background_pink
+	background_projectsloth
+	background_red
+	background_yellow
+]]
+
+local background = "background_darkerblue"
+local opacity = 100
+
+CreateThread(function()
+    RequestStreamedTextureDict('ps_pause', true)
+    while not HasStreamedTextureDictLoaded("ps_pause") do
+        Wait(100)
+    end
+    while true do
+        if IsPauseMenuActive() then
+            SetScriptGfxDrawBehindPausemenu(true)
+            DrawSprite("ps_pause", background, 0.5, 0.5, 10.0, 10.0, 0, 255, 255, 255, opacity)
+        else
+            SetStreamedTextureDictAsNoLongerNeeded('ps_pause')
+        end
+      	Wait(0)
+    end
+end)
+
