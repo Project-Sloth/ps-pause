37698cf018e2ca933cb594630f63625facd19b84
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2023-12-24 21:55:32 +0100

    added config.lua
    added config.lua

---
+++ config.lua
@@ -0,0 +1,36 @@
+Config = Config or {}
+
+----------------------------------------------------------------------------------------------------
+-- # BACKGROUND SECTION # --
+----------------------------------------------------------------------------------------------------
+-- Allows you to set the preferred background Color from a list of available options.
+Config.Background = "background_projectsloth"
+
+-- Available Options --
+-- background_blue
+-- background_darkblue
+-- background_darkerblue
+-- background_darkgreen
+-- background_green
+-- background_other
+-- background_pink
+-- background_projectsloth
+-- background_red
+-- background_yellow
+
+-- Allows you to change the opactiy of the Background
+Config.Opacity = 100
+
+----------------------------------------------------------------------------------------------------
+-- # HEADER and OPTIONS SECTION # --
+----------------------------------------------------------------------------------------------------
+
+Config.Header = {
+    -- LEFT MENU CONFIG
+    ["TITLE"] = "Project Sloth",
+    ["SUBTITLE"] = "This little Script was made by Jay <3",
+    -- RIGHT MENU CONFIG
+    ["SERVER_NAME"] = "Project Sloth",
+    ["SERVER_TEXT"] = "LeSiiN is cool",
+    ["SERVER_DISCORD"] = "YOUR DISCORD HERE"
+}
